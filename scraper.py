4df4c84fcb3eff8bd6b57ba2478ca8da72cc619c
jhengy <jhengy@github.com>
2025-02-02 01:52:34 +0800

    Fix bug unable to load dynamic content

--- scraper.py
+++ scraper.py
@@ -33,7 +33,7 @@ HEADERS = {
 # TODO: manage session creation and closing at the top level, maybe creating a class to encapsulate the session
 # TODO: see if changing to production grade browser automation tools such as playwright or puppeteer would be better in (1) performance (2) reliability (counter measure for anti-scraping measures) e.g. Enable JavaScript and cookies to continue 
 
-async def get_page_html(url, wait_until='domcontentloaded', timeout=SESSION_GET_TIMEOUT):
+async def get_page_html(url, wait_until='networkidle', timeout=SESSION_GET_TIMEOUT):
     """Shared Playwright helper to get rendered HTML"""
     async with async_playwright() as p:
         browser = await p.chromium.launch(
@@ -47,7 +47,13 @@ async def get_page_html(url, wait_until='domcontentloaded', timeout=SESSION_GET_
                 java_script_enabled=True
             )
             page = await context.new_page()
-            await page.goto(url, wait_until=wait_until, timeout=timeout * 1000)
+            await page.goto(url, wait_until="domcontentloaded", timeout=timeout * 1000)
+            
+            try:
+                await page.wait_for_load_state(wait_until, timeout=timeout * 1000)
+            except Exception as e:
+                print(f"Error waiting for networkidle under {url}, may be due to constant background network activity such as analytics, will continue: {str(e)}")
+        
             return await page.content()
         finally:
             await browser.close()
@@ -103,6 +109,7 @@ async def extract_from_index(root_url, css_selector=None, class_name=None,
                  include_patterns=None, exclude_patterns=None):
     try:
         html = await get_page_html(root_url)
+        print(f"=== extract_from_index html ===\n {html} \n=== end of extract_from_index html ===\n") if os.getenv('DEBUG') == 'true' else None
         
         soup = BeautifulSoup(html, 'html.parser')
         links = soup.find_all('a', href=True)
@@ -180,6 +187,15 @@ async def extract_from_rss(rss_url):
 
 async def main():
     # TODO: add support for filtering by date and remove noisy links
+    hn_posts = await extract_from_index(
+        "https://hn.algolia.com/?dateRange=last24h&type=story",
+        **
+        {
+            'css_selector': 'a[href].Story_link',
+        }
+    )
+    print(f"=== hn_posts ===\n {hn_posts} \n=== end of hn_posts ===\n")
+    
     pe_posts = await extract_from_index(
         "https://blog.pragmaticengineer.com",
         **
