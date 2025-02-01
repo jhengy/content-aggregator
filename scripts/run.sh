9b333a27f84889bda20fe8b4665a7babdb21d05b
jhengy <jhengy@github.com>
2025-02-02 00:49:52 +0800

    Update run.sh to clean up before generating new files

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,6 @@ set -ex
 
 app_root=$(dirname "$(dirname "$(realpath "$0")")")
 
+rm -rf $app_root/outputs/*
 python $app_root/main.py
-bash $app_root/scripts/create_issue_body.sh
+bash $app_root/scripts/create_issue_body.sh $app_root/outputs/issue.md
